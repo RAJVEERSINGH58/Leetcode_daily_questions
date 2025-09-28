c7da2b4d43d862fc0c6beac1932992c2ce15855e
Rajveer Singh <pheonixrajveer@gmail.com>
2025-09-28 09:28:29 +0530

    day 62

---
+++ Array/largestPerimeter.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<int>()); 
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            int a = nums[i], b = nums[i+1], c = nums[i+2];
+            if (b + c > a) { 
+                return a + b + c;
+            }
+        }
+
+        return 0;
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
