436634cd08e0bb107c64ca1d6a4c7c0dec64f3e9
Rajveer Singh <pheonixrajveer@gmail.com>
2025-10-18 22:46:43 +0530

    day 82

---
+++ sorting/LC3397.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        sort(begin(nums), end(nums));
+
+        int count = 0;
+
+        int prev = INT_MIN;
+
+        for(int i = 0; i < n; i++) {
+            int minVal = nums[i] - k;
+
+            if(prev < minVal) {
+                //nums[i] = nums[i]-k;
+                prev = minVal;
+                count++;
+            } else if(prev < nums[i] + k) {
+                //nums[i] = prev+1;
+                prev = prev+1;
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+
+
+
+int main() {
+    
+    return 0;
+}
